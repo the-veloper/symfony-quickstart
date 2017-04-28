7dd78db02e5ffd4855402a3460b83a6d6d42e3cf
Georgi Georgiev <georgi.georgiev@ffwagency.com>
2017-04-28 23:47:52 +0300

    login

---
+++ src/Shop/UserBundle/Controller/LoginController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Shop\UserBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\BrowserKit\Request;
+
+class LoginController extends Controller
+{
+  /**
+   * @Cache(maxage=0)
+   * @Route("/login", name="login")
+   */
+  public function LoginAction($route)
+  {
+    $last_username = $this->get('security.authentication_utils')->getLastUsername();
+
+    return $this->render('UserBundle:Security:login.html.twig', [
+      'last_username' => $last_username,
+      'route'         => $route,
+    ]);
+  }
+}
