9c1a43c249f8e4ce5b1c2b5226f643563d4040e7
Georgi Georgiev <georgi.georgiev@ffwagency.com>
2017-04-29 21:15:49 +0300

    buttton fix

--- web/js/product.js
+++ web/js/product.js
@@ -1,13 +1,15 @@
-$('#add_to_card').click(function() {
-    $.ajax({
-        method: "POST",
-        data: {
-            product_id: id,
-            product_qty: 1
-        },
-        url: "{{ path('cart_add') }}",
-        success: function (response) {
-            console.log(response);
-        }
+$(document).ready(function () {
+    $("#add_to_card").click(function () {
+        $.ajax({
+            method: "POST",
+            data: {
+                product_id: $(this).data('id'),
+                product_qty: 1
+            },
+            url: "{{ path('cart_add') }}",
+            success: function (response) {
+                console.log(response);
+            }
+        });
     });
 });
