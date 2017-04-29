d796ed992bcd769a8d49ad51bf6a69d13d7aa87b
Georgi Georgiev <georgi.georgiev@ffwagency.com>
2017-04-30 00:00:00 +0300

    js fix

--- web/js/admin.js
+++ web/js/admin.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $(".js-processed-actions").click(function () {
         $.ajax({
             method: "GET",
-            url: $(this).data('path') + '/' + $(this).data('id'),
+            url: $(this).data('path'),
             success: function (response) {
                 $(this).html(response.new_text);
                 if(response.delete) {
