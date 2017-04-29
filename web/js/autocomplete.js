903da18ef738a84edd129b903cb91d754c91051d
Georgi Georgiev <georgi.georgiev@ffwagency.com>
2017-04-30 02:39:12 +0300

    auto

--- web/js/autocomplete.js
+++ web/js/autocomplete.js
@@ -1,23 +1,25 @@
-$(".autocomplete").autocomplete({
-    minLength: 2,
-    scrollHeight: 220,
-    source: function(req, add){
-        $.ajax({
-            url:'/app_dev.php/category/autocomplete',
-            type:"get",
-            dataType: 'json',
-            data: 'term='+req.term,
-            async: true,
-            cache: true,
-            success: function(data){
-                var suggestions = [];
-                //process response
-                $.each(data, function(i, val){
-                    suggestions.push({"name": val.categoryName});
-                });
-                //pass array to callback
-                add(suggestions);
-            }
-        });
-    }
+$(function() {
+    $(".autocomplete").autocomplete({
+        minLength: 2,
+        scrollHeight: 220,
+        source: function(req, add){
+            $.ajax({
+                url:'/app_dev.php/category/autocomplete',
+                type:"get",
+                dataType: 'json',
+                data: 'term='+req.term,
+                async: true,
+                cache: true,
+                success: function(data){
+                    var suggestions = [];
+                    //process response
+                    $.each(data, function(i, val){
+                        suggestions.push({"name": val.categoryName});
+                    });
+                    //pass array to callback
+                    add(suggestions);
+                }
+            });
+        }
+    });
 });
