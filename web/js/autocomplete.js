2612d8d291c014a213c47e2185108ca5579157e7
Georgi Georgiev <georgi.georgiev@ffwagency.com>
2017-04-30 02:22:52 +0300

    auto

--- web/js/autocomplete.js
+++ web/js/autocomplete.js
@@ -1,4 +1,33 @@
-$( ".autocomplete" ).autocomplete({
-    source: $( ".autocomplete" ).data('source'),
-    minLength: 2,
+$(function() {
+    function log( message ) {
+        $( "<div>" ).text( message ).prependTo( "#log" );
+        $( "#log" ).scrollTop( 0 );
+    }
+
+    $( ".autocomplete" ).autocomplete({
+        source: function( request, response ) {
+            $.ajax({
+                url: $( ".autocomplete" ).data('source'),
+                dataType: "jsonp",
+                data: {
+                    q: request.term
+                },
+                success: function( data ) {
+                    response( data );
+                }
+            });
+        },
+        minLength: 3,
+        select: function( event, ui ) {
+            log( ui.item ?
+                "Selected: " + ui.item.label :
+                "Nothing selected, input was " + this.value);
+        },
+        open: function() {
+            $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
+        },
+        close: function() {
+            $( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
+        }
+    });
 });
