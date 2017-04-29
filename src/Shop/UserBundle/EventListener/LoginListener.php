e10280cecb7c1d5051fdc219049546cd30191eec
Georgi Georgiev <georgi.georgiev@ffwagency.com>
2017-04-29 22:42:08 +0300

    event listener

---
+++ src/Shop/UserBundle/EventListener/LoginListener.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * This file is part of the FOSUserBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Shop\UserBundle\EventListener;
+
+use FOS\UserBundle\Event\UserEvent;
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Model\UserInterface;
+use FOS\UserBundle\Model\UserManagerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+use Symfony\Component\Security\Http\SecurityEvents;
+
+class LoginListener implements EventSubscriberInterface
+{
+    protected $userManager;
+
+    /**
+     * LastLoginListener constructor.
+     *
+     * @param UserManagerInterface $userManager
+     */
+    public function __construct(UserManagerInterface $userManager)
+    {
+        $this->userManager = $userManager;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+          FOSUserEvents::SECURITY_IMPLICIT_LOGIN => 'onImplicitLogin',
+          SecurityEvents::INTERACTIVE_LOGIN => 'onSecurityInteractiveLogin',
+        );
+    }
+
+    /**
+     * @param UserEvent $event
+     */
+    public function onImplicitLogin(UserEvent $event)
+    {
+        $user = $event->getUser();
+        $ip = $event->getRequest()->getClientIp();
+
+        $user->setipAddress($ip);
+        $this->userManager->updateUser($user);
+    }
+
+    /**
+     * @param InteractiveLoginEvent $event
+     */
+    public function onSecurityInteractiveLogin(InteractiveLoginEvent $event)
+    {
+        $user = $event->getAuthenticationToken()->getUser();
+
+        if ($user instanceof UserInterface) {
+            $ip = $event->getRequest()->getClientIp();
+            $user->setipAddress($ip);
+            $this->userManager->updateUser($user);
+        }
+    }
+}
