838b117eead86663d523cee46e513e1bc92fa6fa
Georgi Georgiev <georgi.georgiev@ffwagency.com>
2017-04-29 16:23:54 +0300

    cart view

--- src/AppBundle/Controller/CartController.php
+++ src/AppBundle/Controller/CartController.php
@@ -42,23 +42,13 @@ class CartController extends Controller
     }
 
     /**
-     * View card.
+     * View cart.
      *
      * @Route("/view", name="view_cart")
      * @Method("GET")
      */
     public function viewCartAction(Request $request)
     {
-        $session = new Session();
-        $session->start();
-        $current_total = $session->get('current_total', 0);
-        $product_id = $request->query->get('product_id');
-        $product_qty = $request->query->get('product_qty');
-        $product = $this->getDoctrine()
-          ->getRepository('AppBundle:Product')
-          ->find($product_id);
-        $current_total += $product->price * $product_qty;
-
-        return new JsonResponse(['success' => true, 'new_total' => $current_total]);
+        return new Response("bla-bla");
     }
 }
