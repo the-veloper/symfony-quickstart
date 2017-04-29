9202075ce9799d86c8fe9fbe39c238a821e5a3ae
Georgi Georgiev <georgi.georgiev@ffwagency.com>
2017-04-29 18:42:11 +0300

    cart change endpoint

--- src/AppBundle/Controller/CartController.php
+++ src/AppBundle/Controller/CartController.php
@@ -34,7 +34,11 @@ class CartController extends Controller
         $current_total = $session->get('current_total', 0);
         $product_id = $request->request->get('product_id');
         $product_qty = $request->request->get('product_qty');
-
+        if($product_qty < 0) {
+            return new JsonResponse(
+              ['success' => false, 'new_total' => $current_total]
+            );
+        }
         $product = $this->getDoctrine()
           ->getRepository('AppBundle:Product')
           ->find($product_id);
@@ -81,6 +85,50 @@ class CartController extends Controller
             'products' => $products_renderable,
             'total' => $session->get('current_total', 0),
           )
+        );
+    }
+
+    /**
+     * Remove from cart.
+     *
+     * @Route("/change", name="view_cart")
+     * @Method("POST")
+     */
+    public function changeCartAction(Request $request)
+    {
+        $session = $request->getSession();
+        $current_total = $session->get('current_total', 0);
+        $products = $session->get('products', []);
+        $product_id = $request->request->get('product_id');
+        $product_qty = $request->request->get('product_qty');
+        if($product_qty < 0) {
+            return new JsonResponse(
+              ['success' => false, 'new_total' => $current_total]
+            );
+        }
+        if($product_qty < $products[$product_id]['qty']) {
+            if (isset($products[$product_id])) {
+
+                if ($product_qty == $products[$product_id]['qty']) {
+                    $current_total -= $products[$product_id]['product']->getPrice(
+                      ) * $product_qty;
+                    unset($products[$product_id]);
+                } else {
+                    $products[$product_id]['qty'] -= $product_qty;
+                    $current_total -= $products[$product_id]['product']->getPrice(
+                      ) * $product_qty;
+                }
+            }
+        } else {
+            $request->request->set('product_qty', $product_qty - $products[$product_id]['qty']);
+            return $this->addToCartAction($request);
+        }
+
+        $session->set('current_total', $current_total);
+        $session->set('products', $products);
+
+        return new JsonResponse(
+          ['success' => true, 'new_total' => $current_total]
         );
     }
 }
